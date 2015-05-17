62c660075cb4612a5d0a575a4a0095fb3a074504
Trini Morrison <TRPMorrison@gmail.com>
2015-05-17 17:11:45 -0400

    UPDATES TO ANSWERS

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var answer1 = document.querySelector('#answer1');
 
 answer1.innerText = '';
 
-items.forEach( function (item) {
-  answer1.appendChild(document.createTextNode(item.price + '\n'));
+items.forEach(function(item) {
+    answer1.appendChild(document.createTextNode(item.price + '\n'));
 });
 
 
@@ -12,13 +12,13 @@ items.forEach( function (item) {
 //       <h4>1. What is the average price of all items?</h4>
 //       <pre id="answer1">Answer 1 Goes Here</pre>
 //     </div>
-var allPrices = items.map( function (item) {
-  return item.price;
+var allPrices = items.map(function(item) {
+    return item.price;
 });
 
 
-var sum = allPrices.reduce( function (previousValue, currentValue) {
-  return previousValue + currentValue;
+var sum = allPrices.reduce(function(previousValue, currentValue) {
+    return previousValue + currentValue;
 });
 
 var average = sum / allPrices.length;
@@ -32,26 +32,32 @@ var answer1 = document.querySelector('#answer1').textContent = "The average pric
 //       <h4>2. Show me an array of items that cost between $14.00 and $18.00 USD?</h4>
 //       <pre id="answer2">Answer 2 Goes Here</pre>
 //     </div>
-
+function greaterThan(x, y) {
+    if (y > x) {
+        console.log('True');
+    } else {
+        console.log('False');
+    }
+};
 
 var answer2 = document.querySelector('#answer2');
 
 answer2.innerText = '';
 
-items.forEach( function (item) {
-  answer2.appendChild(document.createTextNode(item.price + item.title + '\n'));
+items.forEach(function(item) {
+    answer2.appendChild(document.createTextNode(item.price + item.title + '\n'));
 });
 
-var allTitles = items.map( function (item) {
-  return item.title;
+var allTitles = items.map(function(item) {
+    return item.title;
 });
 
-var allPrices = items.map( function (item) {
-  return item.price;
+var allPrices = items.map(function(item) {
+    return item.price;
 });
 
-var titlesPrices = items.map( function (item) {
-  return item.price + " " + item.title;
+var titlesPrices = items.map(function(item) {
+    return item.price + " " + item.title;
 });
 
 var answer = average.toFixed(2);
@@ -67,12 +73,12 @@ var answer3 = document.querySelector('#answer3');
 
 answer3.innerText = '';
 
-items.forEach( function (item) {
-  answer3.appendChild(document.createTextNode(item.currency_code + item.title + item.price + '\n'));
+items.forEach(function(item) {
+    answer3.appendChild(document.createTextNode(item.currency_code + item.title + item.price + '\n'));
 });
 
-var newItems = items.filter ( function (item) {
-return item.currency_code === "GBP";
+var newItems = items.filter(function(item) {
+    return item.currency_code === "GBP";
 });
 
 newItems[0].price;
@@ -80,6 +86,8 @@ newItems[0].price;
 var answer3 = newItems[0].title + " £" + newItems[0].price;
 
 document.querySelector('#answer3').textContent = answer3;
+
+console.log(answer3);
 
 //     <div class="question">
 //       <h4>4. Display a list of all items who are made of wood.</h4>
@@ -90,32 +98,46 @@ var answer4 = document.querySelector('#answer4');
 
 answer4.innerText = '';
 
-items.forEach( function (item) {
-  answer4.appendChild(document.createTextNode(item.title + item. + '\n'));
+items.forEach(function(item) {
+    answer4.appendChild(document.createTextNode(item.title + item.materials + '\n'));
 });
+
 
 var allWood = items.map(function(item) {
-  return item.title + item.materials;
+    return item.materials + item.title + '\n';
 });
 
-var answer = average.toFixed(2);
+var wood = items.filter(function(item) {
+    return item.materials === "wood";
+});
 
-document.querySelector('#answer4').textContent = answer + " is made out of wood.";
+
+function filter(item) {
+    if (item.materials === "wood") {
+        console.log('item.title');
+    } else {
+        console.log('False');
+    }
+};
+
+// var answer = ;
+
+// document.querySelector('#answer4').textContent = answer + " is made out of wood.";
 
 //     <div class="question">
 //       <h4>5. Which items are made of eight or more materials? Display the name, number of items and the items it is made of.</h4>
 //       <pre id="answer5">Answer 5 Goes Here</pre>
 //     </div>
 
-  var answer5 = document.querySelector('#answer5');
+var answer5 = document.querySelector('#answer5');
 
-  answer5.innerText = '';
+answer5.innerText = '';
 
-  items.forEach( function (item) {
+items.forEach(function(item) {
     answer5.appendChild(document.createTextNode(item.price + item.title + '\n'));
-  });
+});
 
-  var answer = average.toFixed(2);
+var answer = average.toFixed(2);
 
 document.querySelector('#answer5').textContent = "The average price is $" + answer;
 
@@ -127,8 +149,8 @@ var answer6 = document.querySelector('#answer6');
 
 answer6.innerText = '';
 
-items.forEach( function (item) {
-  answer6.appendChild(document.createTextNode(item.price + item.title + '\n'));
+items.forEach(function(item) {
+    answer6.appendChild(document.createTextNode(item.price + item.title + '\n'));
 });
 
 var answer = average.toFixed(2);
